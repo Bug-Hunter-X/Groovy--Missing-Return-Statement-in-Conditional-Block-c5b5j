c80cb43a92d32d589c9d17fc3bf11c2f446c9852
BugHunter-X <askzenai@gmail.com>
2024-12-13 10:30:05 +0100

    Add bug.groovy file

---
+++ bug.groovy
@@ -0,0 +1,9 @@
+```groovy
+def myMethod(param) {
+  if (param == null) {
+    // Handle null input
+    return defaultValue  // Missing return statement here!
+  }
+  // ... rest of the method
+}
+```
